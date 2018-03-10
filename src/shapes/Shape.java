003af792e62f48f049beba2c4e862a2247015c99
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2018-03-09 20:32:20 -0500

    added simple mechanics

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -2,65 +2,130 @@ package shapes;
 import hsa.Console;
 import java.awt.*;
 public class Shape {
-	private String type;
-	private int xPos, yPos, w, h;
-	private Console console;
-	private Color colour;
-	private Color stroke_colour;
-	
-	/*** CONSTRUCTORS ***/
-	public Shape(Console c, String type, int x, int y, int w, int h) {
-		this.type = type;
-		this.xPos = x;
-		this.yPos = y;
-		this.w = w;
-		this.h = h;
-		this.console = c;
-		this.colour = new Color(255, 255, 255, 255); //hopefully transparent
-		this.stroke_colour = new Color(0,0,0);
-	}
-	public Shape(Console c, String type, int x, int y, int w, int h, Color colour) {
-		this.type = type;
-		this.xPos = x;
-		this.yPos = y;
-		this.w = w;
-		this.h = h;
-		this.console = c;
-		this.colour = colour;
-		this.stroke_colour = new Color(0,0,0);
-	}
-	
-	/*** METHODS ***/
-	public void display() {
-		if(this.type == "rectangle") { 
-			this.console.setColor(this.stroke_colour);
-			this.console.drawRect(this.xPos, this.yPos, this.w, this.h);
-			this.console.setColor(this.colour);
-			this.console.fillRect(this.xPos, this.yPos, this.w, this.h);
-		}
-		else if(this.type == "circle") {
-			this.console.setColor(this.stroke_colour);
-			this.console.drawOval(this.xPos, this.yPos, this.w, this.h);
-			this.console.setColor(this.colour);
-			this.console.fillOval(this.xPos, this.yPos, this.w, this.h);
-		}
-		else if(this.type == "star") {
-			this.console.setColor(this.stroke_colour);
-			this.console.drawStar(this.xPos, this.yPos, this.w, this.h);
-			this.console.setColor(this.colour);
-			this.console.fillStar(this.xPos, this.yPos, this.w, this.h);
-		}
-	}
-	public void stroke(Color s) {
-		this.stroke_colour = s;
-	}
-	public void fill(Color c) {
-		this.colour = c;
-	}
-	public void setSize(int size) {}
-	public void setSize(int w, int h) {	}
-	public void setPosition() {}
-	public void changePosition(){}
-
+	 private String type;
+	 private int xPos, yPos, w, h;
+	 private Console console;
+	 private Color colour;
+	 private Color stroke_colour;
+	 
+	 /*** CONSTRUCTORS ***/
+	 public Shape(Console c, String type, int x, int y, int w, int h) {
+		 this.type = type;
+		 this.xPos = x;
+		 this.yPos = y;
+		 this.w = w;
+		 this.h = h;
+		 this.console = c;
+		 this.colour = new Color(255, 255, 255, 255); //hopefully transparent
+		 this.stroke_colour = new Color(0,0,0);
+	 }
+	 public Shape(Console c, String type, int x, int y, int w, int h, Color colour) {
+		 this.type = type;
+		 this.xPos = x;
+		 this.yPos = y;
+		 this.w = w;
+		 this.h = h;
+		 this.console = c;
+		 this.colour = colour;
+		 this.stroke_colour = new Color(0,0,0);
+	 }
+	 
+	 /*** METHODS ***/
+	 public void display() {
+		 if(this.type == "rectangle") { 
+			 Shape.drawRectangle(this.console, this.xPos, this.yPos, this.w, this.h, this.colour, this.stroke_colour);
+		 }
+		 else if(this.type == "ellipse") {
+			 Shape.drawEllipse(this.console, this.xPos, this.yPos, this.w, this.h, this.colour, this.stroke_colour);
+		 }
+		 else if(this.type == "star") {
+			 Shape.drawStar(this.console, this.xPos, this.yPos, this.w, this.h, this.colour, this.stroke_colour);
+		 }
+	 }
+	 public void stroke(Color s) {
+		 this.stroke_colour = s;
+	 }
+	 public void noStroke() {
+		 this.stroke_colour = new Color(255,255,255,255);//hopefully transparent
+	 }
+	 public void fill(Color c) {
+		 this.colour = c;
+	 }
+	 public void setSize(int size) {
+		 this.w = this.h = size;
+	 }
+	 public void setSize(int w, int h) {
+		 this.w = w;
+		 this.h = h;
+	 }
+	 public void setPosition(int x, int y) {
+		 this.xPos = x;
+	  	 this.yPos = y;
+	 }
+	 public void setType(String type){
+		 this.type = type;
+	 }
+	 public void changePosition(int x, int y){
+		 this.xPos += x;
+		 this.yPos += y;
+	 }
+	 
+	 public static void drawEllipse(Console console, int x, int y, int w, int h) {
+		console.setColor(new Color(255, 255, 255, 255));
+	  	console.fillOval(x, y, w, h);
+	  	console.setColor(new Color(0, 0, 0));
+	  	console.drawOval(x, y, w, h);
+	 }
+	 public static void drawEllipse(Console console, int x, int y, int w, int h, Color colour) {
+		console.setColor(colour);
+		console.fillOval(x, y, w, h);
+		console.setColor(new Color(0, 0, 0));
+		console.drawOval(x, y, w, h);
+	 }
+	 public static void drawEllipse(Console console, int x, int y, int w, int h, Color colour, Color stroke_colour) {
+		console.setColor(colour);
+		console.fillOval(x, y, w, h);
+		console.setColor(stroke_colour);
+		console.drawOval(x, y, w, h);
+	 }
+	 
+	 public static void drawRectangle(Console console, int x, int y, int w, int h) {
+		console.setColor(new Color(255, 255, 255, 255));
+		console.fillRect(x, y, w, h);
+		console.setColor(new Color(0, 0, 0));
+		console.drawRect(x, y, w, h);
+	 }
+	 public static void drawRectangle(Console console, int x, int y, int w, int h, Color colour) {
+		console.setColor(colour);
+		console.fillRect(x, y, w, h);
+		console.setColor(new Color(0, 0, 0));
+		console.drawRect(x, y, w, h);
+	 }
+	 public static void drawRectangle(Console console , int x, int y, int w, int h, Color colour, Color stroke_colour) {
+		console.setColor(colour);
+		console.fillRect(x, y, w, h);
+		console.setColor(stroke_colour);
+		console.drawRect(x, y, w, h);
+	 }
+	 
+	 public static void drawStar(Console console , int x, int y, int w, int h) {
+		console.setColor(new Color(255, 255, 255, 255));
+		console.fillStar(x, y, w, h);
+		console.setColor(new Color(0, 0, 0));
+		console.drawRect(x, y, w, h);
+	 }
+	 public static void drawStar(Console console , int x, int y, int w, int h, Color colour) {
+		console.setColor(colour);
+		console.fillStar(x, y, w, h);
+		console.setColor(new Color(0, 0, 0));
+		console.drawStar(x, y, w, h);
+	 }
+	 public static void drawStar(Console console , int x, int y, int w, int h, Color colour, Color stroke_colour) {
+		console.setColor(colour);
+		console.fillStar(x, y, w, h);
+		console.setColor(stroke_colour);
+		console.drawStar(x, y, w, h);
+	 }
+ 
 }
 
--- src/shapes/SlotMachine.java
+++ src/shapes/SlotMachine.java
@@ -1,5 +1,58 @@
 package shapes;
-
+import hsa.Console;
+import java.awt.*;
+import java.util.Random;
 public class SlotMachine {
-
+	public static Shape shape1;
+	public static Shape shape2;
+	public static Shape shape3;
+	public static Console console;
+	public static boolean running;
+	public static boolean turning;
+	public static Random randNum;
+	public static double timer;
+ 
+	public static void main(String []a) throws InterruptedException {
+		console = new Console(40, 100, 20);
+		randNum = new Random();
+		final String []shapes = {"ellipse", "rectangle", "star"};
+		running = true;
+		turning = true;
+		shape1 = new Shape(console, "ellipse", 0, 0, 100, 100);
+		shape2 = new Shape(console, "rectangle", 200, 0, 100, 100);
+		shape3 = new Shape(console, "star", 400, 0, 100, 100);
+		timer = 0;
+		int input;
+		while(running){
+			if(!turning){
+				input = console.getChar();
+		        if(input== 10){
+		        		turning = true;
+		        		timer = 0;
+		        } else if(input==32){
+		            running = false;
+		        }
+			}else{
+				shape1.fill(new Color(randNum.nextInt(255),randNum.nextInt(255),randNum.nextInt(255)));
+		        shape1.setType(shapes[randNum.nextInt(3)]);
+		        shape2.fill(new Color(randNum.nextInt(255),randNum.nextInt(255),randNum.nextInt(255)));
+		        shape2.setType(shapes[randNum.nextInt(3)]);
+		        shape3.fill(new Color(randNum.nextInt(255),randNum.nextInt(255),randNum.nextInt(255)));
+		        shape3.setType(shapes[randNum.nextInt(3)]);
+		        Thread.sleep((int)(timer*1000));
+		        console.clear();
+		        timer+=0.05;
+		        shape1.display();
+		        shape2.display();
+		        shape3.display();
+		        if(timer >= 0.75){
+		            turning = false;
+		            console.setColor(new Color(255,0,0));
+		            console.drawString("to try again press enter", 0, 200);
+		            console.setColor(new Color(0,0,0,0));
+		        }
+		     }
+		  }
+		  console.close();
+ }
 }
